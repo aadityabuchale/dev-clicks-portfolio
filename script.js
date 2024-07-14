607b1897def1a94f7edafa328f810cd2d9ad7bcc
Aaditya Buchale <aadityabuchale15@gmail.com>
2024-07-14 22:13:07 +0530

    added click event

---
+++ script.js
@@ -0,0 +1,6 @@
+const cardImg = document.querySelector(".card__profile");
+const cardTooltip = document.querySelector(".card__tooltip");
+
+cardImg.addEventListener("click", () => {
+	cardTooltip.classList.toggle("show-tooltip");
+});
